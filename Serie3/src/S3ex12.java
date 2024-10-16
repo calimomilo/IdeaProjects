e8be4d0c9823b3b3ea54e0b8629dcfa5c344e254
Camilo <paula@pena-parra.ch>
2024-10-16 09:20:06 +0200

    modif ex12

--- Serie3/src/S3ex12.java
+++ Serie3/src/S3ex12.java
@@ -1,33 +1,15 @@
-import ch.comem.Clavier;
-
 public class S3ex12 {
     public static void main(String[] args) {
-        byte a = Clavier.rend_byte("Dimension du carré : ");
-        byte b = (byte) (a + 1);
-        int c = a * b;
 
-        if (a % 2 == 0) {
-            for (int i = 2; i <= c; i++) {
-                String msg = (i % 2 == 0)
-                        ? "o"
-                        :"#";
-                System.out.print(msg + "  ");
-                if (i % b == 0) {
-                    i++;
-                    System.out.print("\n");
-                }
-            }
-        } else {
-            for (int i = 1; i <= a*a; i++) {
-                String msg = (i % 2 == 0)
-                        ? "#"
-                        :"o";
-                System.out.print(msg + "  ");
-                if (i % a == 0) {
-                    System.out.print("\n");
-                }
+        for (int i = 2; i <= 72; i++) {
+            String msg = (i % 2 == 0)
+                    ? "o"
+                    :"#";
+            System.out.print(msg + "  ");
+            if (i % 9 == 0) {
+                i++;
+                System.out.print("\n");
             }
         }
-
     }
 }
