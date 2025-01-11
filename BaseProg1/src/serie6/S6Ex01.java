92b09db6479cfc5677f6b06599a4047b7d8e6bef
Camilo <paula@pena-parra.ch>
2025-01-11 18:20:27 +0100

    série 6 ex01 corrections

--- BaseProg1/src/serie6/S6Ex01.java
+++ BaseProg1/src/serie6/S6Ex01.java
@@ -4,8 +4,8 @@ public class S6Ex01 {
 
     public static int indexOf(int[] tab, int nombre) {
         int index = -1;
-        int maxLim = tab.length;
-        int minLim = -1;
+        int maxLim = tab.length-1;
+        int minLim = 0;
         boolean found = false;
 
         do {
@@ -14,12 +14,12 @@ public class S6Ex01 {
                 index = i;
                 found = true;
             } else if (nombre > tab[i]) {
-                minLim = i;
+                minLim = i+1;
             } else {
-                maxLim = i;
+                maxLim = i-1;
             }
 
-            if (maxLim - minLim == 1) {
+            if (maxLim - minLim < 0) {
                 found = true;
             }
 
@@ -30,6 +30,6 @@ public class S6Ex01 {
     public static void main(String[] args) {
         int[] tab = {1, 2, 3, 4, 5, 6, 8, 9, 10};
 
-        System.out.println(indexOf(tab, 10));
+        System.out.println(indexOf(tab, 1));
     }
 }
